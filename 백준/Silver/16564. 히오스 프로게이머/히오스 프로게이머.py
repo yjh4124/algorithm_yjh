c8854a578f44a725ef93bbc887e9c97f58aa5947
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-03-13 19:55:01 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 332 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
@@ -0,0 +1,28 @@
+#16564_히오스 프로그래머_binary search_silver1
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+levels = [ int(input()) for _ in range(n)]
+
+start = min(levels)
+end = start + k
+
+res = 0
+while start <= end:
+    mid = (start + end) // 2
+    
+    hap = 0
+    for level in levels:
+        if mid > level:
+           hap+= (mid - level)
+           
+    if hap <= k:
+        start = mid+1
+        res = max(mid,res)
+    else:
+        end = mid -1
+
+
+print(res)
