d703421153c03faf90185f8bb9d51cd1693a18a6
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-03-16 16:45:12 +0900

    [Gold IV] Title: PPAP, Time: 312 ms, Memory: 44632 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,13 +1,54 @@
-w = input()
-stack = []
-ppap = ["P", "P", "A", "P"]
-for i in range(len(w)):
-    stack.append(w[i])
-    if stack[-4:] == ppap:
-        for _ in range(4):
-            stack.pop()
-        stack.append("P")
-if stack == ppap or stack == ["P"]:
-    print("PPAP")
-else:
-    print("NP")
+from collections import deque
+
+ppap_str = input()
+
+# print(ppap_str)
+
+ppap_que=deque(ppap_str)
+
+stack=[]
+
+check=0
+precheck='A'
+
+for i in ppap_str:
+    
+    if i=='P':
+        stack.append(i)
+
+    elif i=='A':
+        if precheck=='A':
+            print('NP')
+            check=1
+            
+            break
+    
+        elif precheck=='P':
+            if len(stack)!=0:
+                stack.pop()
+
+                if len(stack)!=0:
+                    stack.pop()
+
+                elif len(stack)==0:
+                    print('NP')
+                    check=1
+                    
+                    break
+    precheck=i
+    ppap_que.popleft()
+    # print(stack, ppap_que)
+
+# print(len(ppap_str))
+if check==0: 
+    if stack:
+        if len(stack)==1:
+            if precheck=='P':
+                print('PPAP')
+            elif precheck=='A':
+                print('NP')
+        elif len(stack)>1:
+            print('NP')
+            
+    else: 
+        print('NP')
