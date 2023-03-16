ab2a6531fa143fbc614951c0ed3e0a0a7f3809ef
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-03-16 22:17:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,17 +1,8 @@
-n=int(input())
+import sys
 
-ls=[int(input()) for i in range(n)]
+n=int(sys.stdin.readline())
 
-# print(ls)
+ls=sorted(int(sys.stdin.readline()) for i in range(n))
 
-for i in range(n):
-    for j in range(i+1):
-        if ls[j]>ls[i]:
-            temp=ls[i]
-            for k in range(i,j-1,-1):
-                ls[k]=ls[k-1]
-            ls[j]=temp
-
-# print(ls)
 for i in ls:
     print(i)
