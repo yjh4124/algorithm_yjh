c3eb822db094fa8189d007421c26a361851bfcb7
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-03-23 16:11:25 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35276 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,35 +1,19 @@
 import sys
 from collections import deque
 
-n, k=map(int, sys.stdin.readline().split())
+def bfs(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if v == k:
+            return array[v]
+        for next_v in (v-1, v+1, 2*v):
+            if 0 <= next_v < MAX and not array[next_v]:
+                array[next_v] = array[v] + 1
+                q.append(next_v)
 
 
-queue=deque([n])
-time=0
-
-limit=100000
-visit=[0]*(limit+1)
-visit[n]=1
-
-while queue:
-
-    n=queue.popleft()
-
-    if visit[k] !=0: break
-    if n-1>=0:
-        if visit[n-1] == 0:
-            visit[n-1]=visit[n]+1
-            queue.append(n-1)
-
-    if n+1<=limit:
-        if visit[n+1] == 0:
-            visit[n+1]=visit[n]+1
-            queue.append(n+1)
-
-    if n*2<=limit:
-        if visit[n*2] == 0:
-            visit[n*2]=visit[n]+1
-            queue.append(n*2)
-
-    
-print(visit[k]-1)
+MAX = 100001
+n, k = map(int, sys.stdin.readline().split())
+array = [0] * MAX
+print(bfs(n))
