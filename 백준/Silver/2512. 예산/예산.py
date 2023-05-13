85a907e83f74c06baca886e3cd20c19552767427
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-05-13 11:28:31 +0900

    [Silver III] Title: 예산, Time: 68 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -14,7 +14,7 @@ data.sort()
 sum_=sum(data)
 min_=0
 max_=data[-1]
-# print(sum_)
+
 if sum_<=m:
     print(data[-1])
 
@@ -29,16 +29,15 @@ elif sum_>m:
                 checksum+=mid
             elif elem<=mid:
                 checksum+=elem
-        # print(min_,max_,mid, checksum)
         # print(mid, checksum)
         if checksum<m:
-            if (min_+mid)//2!=min_:
-                min_=(min_+mid)//2
+            if min_!=mid:
+                min_=mid
             else:
-                min_=(min_+mid)//2+1
+                min_=mid+1
 
         elif checksum>m:
-            max_=(mid+max_)//2
+            max_=mid
             
         elif checksum==m:
             break
