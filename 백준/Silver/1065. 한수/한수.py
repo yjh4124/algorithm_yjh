d1a3d12a3034011d56112c078e6a689b08b1fda5
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-25 20:43:14 +0900

    [Silver IV] Title: 한수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -7,7 +7,7 @@ def getHansuCount(num):
         cnt=99
         for i in range(100, num+1):
             i100=i//100
-            i10=i//10-i100*10
+            i10=(i//10)%10
             i1=i%10
             if i10*2==i100+i1: cnt+=1
         return print(cnt)
