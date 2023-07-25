91476873915cb31362fbda879727de060db4e38f
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-25 19:32:13 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,21 @@
-n=int(input())
+N= int(input())
+numSet=set(map(int, input().split()))
 
-numlist=list(map(int, input().split()))
+def makePrimeSet(num):
+    primeNum={i for i in range(2,num+1)}
 
-count=0
+    for i in range(2, int(num**0.5)+1):
+        cnt=0
 
-for i in numlist:
-    checknum=0
-    for j in range(1,i+1):
-        if i%j==0: checknum+=1
-    if checknum==2:
-        count+=1
-        
-print(count)
+        while (i*(i+cnt))<=num:
+            if (i*(i+cnt)) in primeNum: primeNum.remove(i*(i+cnt))
+            cnt+=1
+    
+    return primeNum
+
+primeNum=makePrimeSet(1000)
+
+def getPrimeCount(numSet):
+    return print(len(primeNum & numSet))
+
+getPrimeCount(numSet)
