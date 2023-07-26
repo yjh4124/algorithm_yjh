e4e4a0a137f909167eb8fdc4bf502a97d9cd389c
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-26 16:01:29 +0900

    [Silver IV] Title: 카드 놓기, Time: 52 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,69 +1,19 @@
 n=int(input())
 k=int(input())
-card=[]
-for i in range(0,n):
-    card.append(int(input()))
+cards=[input() for _ in range(0, n)]
 
-# print(card)
+permCards=[]
+idxArr=[0]*(n+1)
 
-# print(list(range(0,n)[::-1]))
-idls=[]
-tpidls=[]
+def makePermCards(k, string_, idxArr):
+    if k==0: permCards.append(string_)
+    elif k>0:
+        for idx in range(0, n):
+            if idxArr[idx]==0:
+                new_idxArr=idxArr.copy()
+                new_idxArr[idx]=1
+                makePermCards(k-1, string_+cards[idx], new_idxArr)
 
-idval=[0 for i in range(0,k)]
+makePermCards(k, '', idxArr)
 
-# if -1 not in idval:
-#     print(idval[0])
-
-idset=[]
-def cardpic(cdn, rek, precnt, cnt, idval,roof):
-    for i in range(0,cdn+1)[::-1]:
-        # roof+=1
-        # print('----')
-        idval[precnt-1]=i-1
-        # print(idval)
-        if i==0: 
-            cnt=cnt-1
-            # print(1)
-            continue
-        else: cnt=precnt
-        # if cnt<rek: cnt+=1  
-        if -1 not in idval:
-            if (idval not in idset):
-                if len(set(idval))==rek:
-                    templs=[]
-                    for i in idval:
-                        templs.append(i)
-                    idset.append(templs)
-                    # print(tuple(templs))
-        if cnt<rek: 
-            cnt+=1
-
-            # print(roof)
-            cardpic(cdn, rek, precnt+1, cnt, idval,roof)
-
-
-# cardpic(n, k, 1, idval,0)
-cardpic(n, k, 1, 1, idval,0)
-
-
-# print(idset)
-
-idlist = idset
-for i in idlist:
-    if i[::-1] not in idlist: idlist.append(i[::-1])
-# print(idlist)
-
-numls=[]
-for j in idlist:
-    temp=''
-    for k in j:
-        temp+=str(card[k])
-    # print(temp)
-    numls.append(temp)
-
-# print(numls)
-realnumls=set(numls)
-
-# print(realnumls)
-print(len(realnumls))
+print(len(set(permCards)))
