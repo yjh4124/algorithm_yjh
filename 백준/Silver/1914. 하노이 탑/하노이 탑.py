9c17fa7712f128373bc53e2d2a9692e0cb15009c
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-26 18:16:29 +0900

    [Silver I] Title: 하노이 탑, Time: 736 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,17 +1,15 @@
 n=int(input())
 
-def move(no, x, y, cnt):
+def move(no, x, y):
     if no>1:
-        move(no-1, x, 6-x-y,cnt)
-        cnt+=1
+        move(no-1, x, 6-x-y)
 
-    
     print(f'{x} {y}')
 
     if no>1:
-        move(no-1, 6-x-y, y,cnt)
+        move(no-1, 6-x-y, y)
 
 print(f'{pow(2,n)-1}')
 
 if n<=20:
-    move(n, 1, 3, 0)
+    move(n, 1, 3)
