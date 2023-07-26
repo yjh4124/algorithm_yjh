3401450435e77632dce902baa7e2a64b4a235ea7
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-26 14:29:41 +0900

    [Bronze V] Title: 팩토리얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -2,7 +2,7 @@ N=int(input())
 
 def factorial(N):
     if N<=1: return 1
-    elif N==2: return 2
-    elif N>=3: return factorial(N-1)*N
+    elif N>=2: return factorial(N-1)*N
 
 print(factorial(N))
+
