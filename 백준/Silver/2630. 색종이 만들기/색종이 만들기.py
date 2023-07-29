45a7e36b2d9725d765dbd83f1396f48c7552096c
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-29 16:41:08 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,26 +1,34 @@
 import sys
 
-N = int(sys.stdin.readline())
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
+n= int(input())
+papers=tuple(tuple(map(int, sys.stdin.readline().split())) for _ in range(n))
 
-result = []
+colorPaper=[0,0]
 
-def solution(x, y, N) :
-  color = paper[x][y]
-  for i in range(x, x+N) :
-    for j in range(y, y+N) :
-      if color != paper[i][j] :
-        solution(x, y, N//2)
-        solution(x, y+N//2, N//2)
-        solution(x+N//2, y, N//2)
-        solution(x+N//2, y+N//2, N//2)
-        return
-  if color == 0 :
-    result.append(0)
-  else :
-    result.append(1)
+def devidePapers(rowStart, rowEnd, colStart, colEnd):
+    global colorPaper
+    
+    initColor=papers[rowStart][colStart]
+    boolPaper=0
 
+    for rowId in range(rowStart, rowEnd):
+        if boolPaper==1: break
+        for colId in range(colStart, colEnd):
+            if papers[rowId][colId]!=initColor: 
+                boolPaper=1
+            if boolPaper==1: break
 
-solution(0,0,N)
-print(result.count(0))
-print(result.count(1))
+    if boolPaper==0: colorPaper[initColor]+=1
+    else: 
+        if(rowEnd-rowStart)//2>=1 and (colEnd-colStart)//2>=1:
+            # 좌상단
+            devidePapers(rowStart, rowStart+(rowEnd-rowStart)//2, colStart, colStart+(colEnd-colStart)//2)
+            # 우상단
+            devidePapers(rowStart, rowStart+(rowEnd-rowStart)//2, colStart+(colEnd-colStart)//2, colEnd)
+            # 우하단
+            devidePapers(rowStart+(rowEnd-rowStart)//2, rowEnd, colStart+(colEnd-colStart)//2, colEnd)
+            # 좌하단
+            devidePapers(rowStart+(rowEnd-rowStart)//2, rowEnd, colStart, colStart+(colEnd-colStart)//2)
+
+devidePapers(0, n, 0, n)
+print(*colorPaper)
