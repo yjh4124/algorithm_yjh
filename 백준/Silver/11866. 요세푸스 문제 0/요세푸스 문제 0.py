30d026dfe471b61960df5bdc9c36327e84d9d731
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-30 17:37:33 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,25 +1,14 @@
 from collections import deque
-n,k=map(int,input().split())
 
-que=deque([i for i in range(1, n+1)])
-ls=[]
-
-# print(que)
-
-while 1:
-    if len(que)==0:
-        break
+n,k=map(int,(input().split()))
+queue=deque(i for i in range(1,n+1))
     
-    ls.append(que[k-1])
-    que.remove(que[k-1])
-    que.rotate(-(k-1))
-    # print(f'{que} {ls}')
+def popKth(k):
+    print('<', end='')
+    while queue:
+        queue.rotate(-(k-1))
+        print(queue.popleft(),end='')
+        if queue: print(', ',end='')
+    print('>')
 
-    if len(que)==k-1:
-        for i in range(len(que)):
-            que.rotate(-(k-1))
-            ls.append(que.popleft())
-
-print('<', end='')
-print(*ls, sep=', ',end='')
-print('>', end='')
+popKth(k)
