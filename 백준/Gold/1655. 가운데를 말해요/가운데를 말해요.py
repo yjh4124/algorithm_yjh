151b244edde4985d2ca39de9cc893e86ea01fc47
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-30 18:41:41 +0900

    [Gold II] Title: 가운데를 말해요, Time: 312 ms, Memory: 37620 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -0,0 +1,28 @@
+from collections import deque
+import heapq
+import sys
+n=int(input())
+heapL=[]
+heapR=[]
+
+def getMidNum():
+    num=int(sys.stdin.readline())
+    if len(heapL)==len(heapR): heapq.heappush(heapL, -num)
+    else: heapq.heappush(heapR, num)
+
+    if heapL and heapR: 
+        tempL=-heapq.heappop(heapL)
+        tempR=heapq.heappop(heapR)
+        if tempL>tempR:
+            heapq.heappush(heapR, tempL)
+            heapq.heappush(heapL, -tempR)
+        else:
+            heapq.heappush(heapR, tempR)
+            heapq.heappush(heapL, -tempL)
+            
+        print(min(-heapL[0], heapR[0]))
+    elif heapL: print(-heapL[0])
+    else: print(heapR[0])
+
+for _ in range(n):
+    getMidNum()
