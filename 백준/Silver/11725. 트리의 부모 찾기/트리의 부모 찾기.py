a4510c369eae97400007197841d1f7c6cf869325
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-07-31 20:11:41 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 328 ms, Memory: 65060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,27 +1,27 @@
 import sys
-from collections import deque
-sys.setrecursionlimit(10**6)
-n = int(sys.stdin.readline())
+sys.setrecursionlimit(10**9)
+n=int(input())
 
-graph = [[] for i in range(n+1)]
+graphList=[[] for _ in range(n+1)]
 
-for i in range(n-1):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+def getGraph():
+    for _ in range(n-1):
+        node1, node2=map(int, sys.stdin.readline().split())
+        graphList[node1].append(node2)
+        graphList[node2].append(node1)
 
-# print(graph)
-visited = [0]*(n+1)
+getGraph()
+
+visited=[0 for _ in range(n+1)]
 visited[1]=1
-arr = []
 
-def dfs(s):
-    for i in graph[s]:
-        if visited[i] == 0:
-            visited[i] = s
-            dfs(i)
+def findNodeParent(parent):
+    for child in graphList[parent]:
+        if visited[child]==0: 
+            visited[child]=parent
+            findNodeParent(child)            
 
-dfs(1)
+findNodeParent(1)
 
-for x in range(2, n+1):
-    print(visited[x])
+for idx in range(2, n+1):
+    print(visited[idx])
