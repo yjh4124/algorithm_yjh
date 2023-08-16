767e0d9fa5cb880dd61217938f947531c077b8a3
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2023-08-16 11:40:09 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.53 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,30 @@
+def solution(today, terms, privacies):
+    
+    year, month, day= map(int, today.split('.'))
+    todayValue= year*28*12 + month*28 + day
+
+    answer = []
+    idx=1
+    for privacy in privacies:
+        expiredValue= getExpiredValue(privacy, terms)
+        
+        if expiredValue<=todayValue:
+            answer.append(idx)
+        idx+=1
+    
+    return answer
+
+def getExpiredValue(privacy, terms):
+    
+    date, term= privacy.split()
+    year, month, day= map(int, date.split('.'))
+    
+    for data in terms:
+        termName, termPeriod= data.split()
+        if termName == term: break            
+    
+    convertedValue= year*28*12 + month*28 + day + int(termPeriod)*28
+    
+    return convertedValue
+
+    
