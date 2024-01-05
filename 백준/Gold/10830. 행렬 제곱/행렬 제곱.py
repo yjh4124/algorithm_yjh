4ac18cff6a8cba7a0717165a5e8d09f3e27f21fe
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-01-05 17:09:25 +0900

    [Gold IV] Title: 행렬 제곱, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,34 +1,57 @@
+
 import sys
 
-a, b = map(int, input().split())
-matrix=tuple(tuple(map(int, sys.stdin.readline().split())) for _ in range(a))
+input = sys.stdin.readline
 
-def getPowMatrix(matrix, mulTimes):
-    if mulTimes==1: 
-        newMatrix=[[0 for _ in range(a)] for _ in range(a)]
-        for rowId in range(a):
-            for colId in range(a):
-                newMatrix[rowId][colId]=matrix[rowId][colId]%1000
-        return newMatrix
-    
-    elif mulTimes==2: 
-        return getMulMatrix(matrix, matrix)
-                
-    else:
-        devidedMulMatrix=getPowMatrix(matrix, mulTimes//2)
-        squareMulMatrix=getPowMatrix(devidedMulMatrix, 2)
-        if mulTimes%2==0: return squareMulMatrix
-        else: 
-            return getMulMatrix(squareMulMatrix, matrix)
 
-def getMulMatrix(matrix_a, matrix_b):
-    newMatrix=[[0 for _ in range(a)] for _ in range(a)]
-    for rowId in range(a):
-        for colId in range(a):
-            for idx in range(a):
-                newMatrix[rowId][colId]+=matrix_a[rowId][idx]*matrix_b[idx][colId]
-                newMatrix[rowId][colId]%=1000
-    return newMatrix
+def getPowMatrix(a_matrix, n, b):
+    if b == 1:
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = a_matrix[i][j] % 1000
+        return matrix
 
-for i in getPowMatrix(matrix, b):
+    if b == 2:
+        return getSquareMatrix(a_matrix, n)
+
+    if b >= 3:
+        if b % 2 == 0:
+            return getSquareMatrix((getPowMatrix(a_matrix, n, b // 2)), n)
+        else:
+            return getMulMatrix((getPowMatrix(a_matrix, n, b - 1)), a_matrix, n)
+
+
+def getSquareMatrix(matrix, n):
+    squareMatrix = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                squareMatrix[i][j] += matrix[i][k] * matrix[k][j]
+            squareMatrix[i][j] %= 1000
+
+    return squareMatrix
+
+
+def getMulMatrix(matrix, a_matrix, n):
+    mulMatrix = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                mulMatrix[i][j] += matrix[i][k] % 1000 * a_matrix[k][j] % 1000
+            mulMatrix[i][j] %= 1000
+
+    return mulMatrix
+
+
+n, b = map(int, input().split())
+
+a_matrix = tuple((tuple(map(int, input().split()))) for _ in range(n))
+
+
+resultMatrix = getPowMatrix(a_matrix, n, b)
+
+for i in resultMatrix:
     print(*i)
