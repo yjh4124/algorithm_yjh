029139cbeba8e3062e0605a879ea779b65e30bec
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-01-05 17:20:23 +0900

    [Gold IV] Title: 행렬 제곱, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,4 +1,3 @@
-
 import sys
 
 input = sys.stdin.readline
@@ -13,34 +12,30 @@ def getPowMatrix(a_matrix, n, b):
         return matrix
 
     if b == 2:
-        return getSquareMatrix(a_matrix, n)
+        return getMulMatrix(a_matrix, a_matrix, n)
 
-    if b >= 3:
-        if b % 2 == 0:
-            return getSquareMatrix((getPowMatrix(a_matrix, n, b // 2)), n)
-        else:
-            return getMulMatrix((getPowMatrix(a_matrix, n, b - 1)), a_matrix, n)
-
-
-def getSquareMatrix(matrix, n):
-    squareMatrix = [[0 for _ in range(n)] for _ in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                squareMatrix[i][j] += matrix[i][k] * matrix[k][j]
-            squareMatrix[i][j] %= 1000
-
-    return squareMatrix
+    halfPowMatrix = getPowMatrix(a_matrix, n, b // 2)
+    if b % 2 == 0:
+        return getMulMatrix(
+            halfPowMatrix,
+            halfPowMatrix,
+            n,
+        )
+    else:
+        return getMulMatrix(getMulMatrix(
+            halfPowMatrix,
+            halfPowMatrix,
+            n,
+        ), a_matrix, n)
 
 
-def getMulMatrix(matrix, a_matrix, n):
+def getMulMatrix(matrix1, matrix2, n):
     mulMatrix = [[0 for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
             for k in range(n):
-                mulMatrix[i][j] += matrix[i][k] % 1000 * a_matrix[k][j] % 1000
+                mulMatrix[i][j] += matrix1[i][k] % 1000 * matrix2[k][j] % 1000
             mulMatrix[i][j] %= 1000
 
     return mulMatrix
