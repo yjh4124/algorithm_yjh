7ac0073d86550f4cde256ea7b448166be2f7e139
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-01-07 17:32:53 +0900

    [Gold II] Title: 철로, Time: 296 ms, Memory: 54332 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.py
+++ 백준/Gold/13334. 철로/철로.py
@@ -1,29 +1,28 @@
 import sys
+import heapq
 
-sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
 
-import heapq
+def getMaxNumRoute(routes, d):
+    maxNumRoute = 0
+    leftHeap = []
+
+    for left, right in routes:
+        while leftHeap and right - leftHeap[0] > d:
+            heapq.heappop(leftHeap)
+        
+        if right - left <= d:
+            heapq.heappush(leftHeap, left)
+            maxNumRoute = max(maxNumRoute, len(leftHeap))
+
+    return maxNumRoute
+
 
 n = int(input())
-trains = []
-
-for _ in range(n):
-    start, end = map(int, input().split())
-    trains.append((min(start, end), max(start, end)))
-
+routes = [sorted(map(int, input().split())) for _ in range(n)]
+routes.sort(key=lambda x: x[1])
 d = int(input())
-trains.sort(key=lambda x: x[1])
-min_heap = []
-count = 0
 
-for train in trains:
-    while min_heap and train[1] - min_heap[0] > d:
-        heapq.heappop(min_heap)
-
-    if train[1] - train[0] <= d:
-        heapq.heappush(min_heap, train[0])
-        count = max(count, len(min_heap))
-
-print(count)
+maxNumRoute = getMaxNumRoute(routes, d)
+print(maxNumRoute)
