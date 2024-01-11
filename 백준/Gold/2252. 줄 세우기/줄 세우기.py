c0e3a8d2f5feb34d4955e92f4905ebeae4322406
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-01-11 17:31:59 +0900

    [Gold III] Title: 줄 세우기, Time: 4124 ms, Memory: 38380 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,27 +1,38 @@
+from collections import deque
 # 입력 받기
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)  # 방문 여부 체크 배열
-result = []  # 결과를 담을 리스트, 스택으로 활용
+indegree = [0] * (n + 1)  # 모든 노드의 진입 차수는 0으로 초기화
 
-# 모든 간선 정보를 입력받아 그래프를 구성
+# 모든 간선 정보를 입력받아 그래프를 구성하고 진입 차수를 증가
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
+    indegree[b] += 1
 
-# DFS 함수
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
-    result.append(v)  # 현재 노드 방문 처리 후 결과 스택에 추가
+# 위상 정렬 함수
+def topology_sort():
+    result = []  # 알고리즘 수행 결과를 담을 리스트
+    q = deque()
 
-# 모든 노드에 대해 DFS 수행
-for i in range(1, n + 1):
-    if not visited[i]:
-        dfs(i)
+    # 처음 시작할 때 진입 차수가 0인 노드를 큐에 삽입
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
 
-# 위상 정렬을 수행한 결과를 역순으로 출력
-for i in result[::-1]:
-    print(i, end=' ')
+    while q:
+        now = q.popleft()
+        result.append(now)
+
+        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+        for i in graph[now]:
+            indegree[i] -= 1
+            # 새롭게 진입 차수가 0이 된 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+
+    # 위상 정렬을 수행한 결과 출력
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
