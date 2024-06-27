48f9b88a7047464d520baa4f80d17126cdbcbcb2
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-06-27 09:57:27 +0900

    Time: 86 ms (75.37%), Space: 13.1 MB (5.87%) - LeetHub

--- LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,25 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        n=len(numbers)
-        
-        # if numbers[0]+numbers[-1]==target:return [1,n]
-        
+        n = len(numbers)
+
         for i in range(n):
-            lidx=i
-            ridx=n-1
-            temp=n-1
-            
-            while lidx<ridx:    
-                # print(lidx, ridx)
-                twosum=numbers[lidx]+numbers[ridx]
-                if twosum>target:
-                    temp=ridx
-                    ridx=(lidx+ridx)//2
-                elif twosum<target:
-                    ridx=(ridx+temp+1)//2
-                    if ridx==temp: break
-                else: 
-                    return [lidx+1, ridx+1]
-                
-                
+            lidx = i
+            ridx = n - 1
+
+            while lidx < ridx:
+                twosum = numbers[lidx] + numbers[ridx]
+                if twosum > target:
+                    ridx -= 1
+                elif twosum < target:
+                    lidx += 1
+                else:
+                    return [lidx + 1, ridx + 1]
+
+        return []
