4e85838f61156b2e992e40dfec6df9a058689739
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-09-02 12:50:03 +0900

    Time: 64 ms (93.59%), Space: 17 MB (6.08%) - LeetHub

--- LeetCode/0037-sudoku-solver/0037-sudoku-solver.py
+++ LeetCode/0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,7 +1,7 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
-        Do not return anything, modify board in-place instead.
+        Solve the Sudoku puzzle by modifying the board in-place.
         """
         
         DIGITS = set("123456789")
@@ -13,11 +13,12 @@ class Solution:
         def initialize_groups(board):
             class Cell:
                 def __init__(self, value):
-                    self.value=value
-          
-            rows=[[] for _ in range(9)]
-            cols=[[] for _ in range(9)]
-            boxes=[[] for _ in range(9)]
+                    self.value = value
+                    self.possible_digits = DIGITS if value == '.' else set()
+
+            rows = [[] for _ in range(9)]
+            cols = [[] for _ in range(9)]
+            boxes = [[] for _ in range(9)]
 
             for i in range(9):
                 for j in range(9):
@@ -28,48 +29,57 @@ class Solution:
                     boxes[k].append(cell)
                 
             return rows, cols, boxes
-            
+        
+        # Update possible digits for a cell
+        def update_possible_digits():
+            for i in range(9):
+                for j in range(9):
+                    cell = rows[i][j]
+                    if cell.value == '.':
+                        k = box_index(i, j)
+                        cell.possible_digits = DIGITS - used_digits(rows[i]) - used_digits(cols[j]) - used_digits(boxes[k])
+
         # Get the used digits in a group
         def used_digits(group):
-            return set(cell.value for cell in group)
+            return {cell.value for cell in group if cell.value != '.'}
         
-        # Find possible digits for a cell
-        def get_possible_digits(i, j):
-            k=box_index(i, j)
-            digits=DIGITS\
-            -used_digits(rows[i])\
-            -used_digits(cols[j])\
-            -used_digits(boxes[k])
-            return digits
+        # Find the cell with the fewest possible digits
+        def find_most_constrained_cell() -> Tuple[int, int]:
+            min_options = 10  # More than the maximum possible options (9)
+            target_cell = None
+            for i in range(9):
+                for j in range(9):
+                    cell = rows[i][j]
+                    if cell.value == '.' and len(cell.possible_digits) < min_options:
+                        min_options = len(cell.possible_digits)
+                        target_cell = (i, j)
+            return target_cell
         
         # dfs to solve the Sudoku
         def dfs_sudoku():
-            for i in range(9):
-                for j in range(9):
-                    cell=rows[i][j]
-                    if cell.value!=".": continue
-                    
-                    for digit in get_possible_digits(i, j):
-                        cell.value=digit
-                        if dfs_sudoku():
-                            return True
-                        cell.value='.'
-                    
-                    return False
-                
-            # If no empty cell left, the board is solved
-            else: 
-                # for row in rows:
-                #     print([cell.value for cell in row])
-                return True
-         
+            cell_pos = find_most_constrained_cell()
+            if not cell_pos:
+                return True  # No cells left to fill, puzzle solved
+            
+            i, j = cell_pos
+            cell = rows[i][j]
+            
+            for digit in cell.possible_digits:
+                cell.value = digit
+                update_possible_digits()
+                if dfs_sudoku():
+                    return True
+                cell.value = '.'
+                update_possible_digits()
+            
+            return False
         
-        rows, cols, boxes =initialize_groups(board)
+        rows, cols, boxes = initialize_groups(board)
+        update_possible_digits()
         
         dfs_sudoku()
-            
+        
+        # Update the board with the solution
         for i in range(9):
             for j in range(9):
-                board[i][j]=rows[i][j].value
-        
-        
+                board[i][j] = rows[i][j].value
