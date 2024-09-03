ab7b030a731f434cc3ccec7736f7b2542bf55b95
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-09-03 11:29:40 +0900

    Time: 47 ms (73.51%), Space: 16.7 MB (27.62%) - LeetHub

--- LeetCode/0980-unique-paths-iii/0980-unique-paths-iii.py
+++ LeetCode/0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -2,22 +2,20 @@ class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         
         rows, cols=len(grid), len(grid[0])
-        visited=[[0]*cols for _ in range(rows)]
         start=[-1,-1]
         empty_cells=0
-        path_cnt=0
-        
-        def count_visited():
-            return sum(sum(row) for row in visited)
         
         def mark_visited(i, j):
-            visited[i][j]=1
+            nonlocal grid
+            grid[i][j]=-1
             
         def unmark_visited(i, j):
-            visited[i][j]=0   
+            nonlocal grid
+            grid[i][j]=0   
         
         def is_visited(i, j):
-            return visited[i][j]
+            nonlocal grid
+            return grid[i][j]==-1
         
       
         def initialize_grid():
@@ -27,30 +25,31 @@ class Solution:
                 for j in range(cols):
                     if grid[i][j]==1:
                         start=[i, j]
-                        mark_visited(i, j)
-                        empty_cells+=1
-                    elif grid[i][j]==0:
+                    if grid[i][j]!=-1:
                         empty_cells+=1
                         
-        def dfs_paths(x, y):
-            nonlocal path_cnt
+        def dfs_paths(x, y, visited_cnt):
+            
+            if grid[x][y] == 2:
+                return visited_cnt == empty_cells
+            
+            path_cnt=0
+            mark_visited(x, y)
             
             # up, right, down, left
             directions=[(-1,0), (0,1), (1, 0), (0, -1)]
             
             for dx, dy in directions:
                 nx, ny= x+dx, y+dy
-                if 0 <= nx < rows and 0 <= ny < cols:
-                    if grid[nx][ny] == 0 and not is_visited(nx, ny):
-                        mark_visited(nx, ny)
-                        dfs_paths(nx, ny)
-                        unmark_visited(nx, ny)
-                    elif grid[nx][ny] == 2 and count_visited() == empty_cells:
-                        path_cnt += 1
-                
-        initialize_grid()
-        dfs_paths(start[0], start[1])
+                if 0 <= nx < rows and 0 <= ny < cols and not is_visited(nx, ny):
+                    path_cnt+=dfs_paths(nx, ny, visited_cnt+1)
+            
+            unmark_visited(x, y)
+            
+            return path_cnt
         
-        return path_cnt
+        initialize_grid()
+        
+        return dfs_paths(start[0], start[1], 1)
         
             
