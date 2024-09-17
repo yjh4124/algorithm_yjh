844a98c4544fb78a494314d1b511d6e4cf6b3d58
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-09-17 16:35:06 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -8,6 +8,24 @@ from collections import deque
 n = int(input())
 
 
+def bfs(x, y):
+    global visited, row, col
+    visited[x][y] = 1
+
+    queue.append((x, y))
+
+    while queue:
+        tmp_x, tmp_y = queue.popleft()
+        for idx in range(4):
+            nx = tmp_x + dx[idx]
+            ny = tmp_y + dy[idx]
+            if 0 <= nx <= row - 1 and 0 <= ny <= col - 1:
+                if visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    if data[nx][ny] == 1:
+                        queue.append((nx, ny))
+
+
 for _ in range(n):
     col, row, cabbage_num = map(int, input().split())
     data = [[0] * col for _ in range(row)]
@@ -16,7 +34,7 @@ for _ in range(n):
         c, r = map(int, input().split())
         data[r][c] = 1
 
-    visit = [[0 for _ in range(col)] for _ in range(row)]
+    visited = [[0 for _ in range(col)] for _ in range(row)]
 
     dx = (-1, 0, 1, 0)
     dy = (0, 1, 0, -1)
@@ -25,35 +43,11 @@ for _ in range(n):
 
     queue = deque()
 
+    cnt = 0
     for x in range(row):
         for y in range(col):
-            cnt = 0
+            if data[x][y] and not visited[x][y]:
+                bfs(x, y)
+                cnt += 1
 
-            if visit[x][y] == 0:
-                visit[x][y] = 1
-
-                if data[x][y] == 0:
-                    continue
-                elif data[x][y] == 1:
-                    queue.append((x, y))
-
-                    while queue:
-                        tmp_x, tmp_y = queue.popleft()
-
-                        cnt += 1
-
-                        for idx in range(4):
-                            nx = tmp_x + dx[idx]
-                            ny = tmp_y + dy[idx]
-                            if 0 <= nx <= row - 1 and 0 <= ny <= col - 1:
-                                if visit[nx][ny] == 0:
-                                    visit[nx][ny] = 1
-                                    if data[nx][ny] == 1:
-                                        queue.append((nx, ny))
-
-                    result.append(cnt)
-
-            elif visit[x][y] == 1:
-                continue
-
-    print(len(result))
+    print(cnt)
