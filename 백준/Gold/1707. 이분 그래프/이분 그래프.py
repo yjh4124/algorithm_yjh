84af4c106d52188be7e5a2f6f452e9e64eb663ed
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-09-17 15:34:56 +0900

    [Gold IV] Title: 이분 그래프, Time: 1224 ms, Memory: 70760 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,55 +1,46 @@
+
 import sys
 
+input = sys.stdin.readline
 sys.setrecursionlimit(10**8)
-k=int(input())
 
-result=True
+k = int(input())
 
-def dfs(node):
-    global result
-    for child in graph[node]:
-        if visit[child]==None:
-            visit[child]=1
-            link.append(child)
-            group[child]=-group[node]
-            dfs(child)
 
-        elif visit[child]!=None:
-            if group[child]==group[node]:
-                result=False
+def is_bipartite_graph(v, graph):
+    visit = group = [0] * (v)
+
+    def dfs(node):
+        for child in graph[node]:
+            if visit[child] == 0:
+                visit[child] = 1
+                group[child] = -group[node]
+                if not dfs(child):
+                    return False
+
+            elif group[child] == group[node]:  # 같은 그룹이면 False 반환
+                return False
+
+        return True
+
+    for j in range(v):
+        if graph[j] and visit[j] == 0:
+            group[j] = 1
+            visit[j] = 1
+            if not dfs(j):
+                return False
+
+    return True
+
 
 for _ in range(k):
-    result=True
-    v, e=map(int, input().split())
+    v, e = map(int, input().split())
 
-    graph=[[] for _ in range(v+1)]
+    graph = [[] for _ in range(v)]
 
     for _ in range(e):
         a, b = map(int, sys.stdin.readline().split())
-        graph[a].append(b)
-        graph[b].append(a)
+        graph[a - 1].append(b - 1)
+        graph[b - 1].append(a - 1)
 
-    visit=[None]*(v+1)
-    visit[0]=0
-    group=[None]*(v+1)
-    group[0]=0
-    # check=0
-    link=[]
-    while None in visit:
-        for j in range(1,len(graph)):
-            if graph[j]!=[] and visit[j]==None:
-                # check=j
-                group[j]=1
-                visit[j]=1
-                link.append(j)
-                dfs(j)
-
-            elif graph[j]==[]:
-                visit[j]=0
-                group[j]=0
-
-    if result==True:
-        print('YES')
-    else: print('NO')
-    # print()
-
+    print("YES") if is_bipartite_graph(v, graph) else print("NO")
