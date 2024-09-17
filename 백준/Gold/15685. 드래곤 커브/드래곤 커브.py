f153c768df18de9e1320667e1c08a93cb0a36c6b
yjh4124 <126434732+yjh4124@users.noreply.github.com>
2024-09-17 16:00:01 +0900

    [Gold III] Title: 드래곤 커브, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,61 +1,58 @@
+
 import sys
 
-n=int(input())
+input = sys.stdin.readline
 
-data=[[int(data) for data in sys.stdin.readline().split()] 
-      for _ in range(n)]
+n = int(input())
 
-# print(data)
+data = [[int(data) for data in sys.stdin.readline().split()] for _ in range(n)]
 
-dx=(0,-1,0,1)
-dy=(1,0,-1,0)
+# 좌표 증가 방향: 우, 상, 좌, 하
+dx = (0, -1, 0, 1)
+dy = (1, 0, -1, 0)
 
-size=101
-graph=[[0 for _ in range(size)] for _ in range(size)]
-
-for y, x, d, g in data:
-    
-    stack=[]
-    stack.append((x,y))
-    stack.append((x+dx[d],y+dy[d]))
-
-    graph[x][y]=1
-    graph[x+dx[d]][y+dy[d]]=1
-    num_point=2
-    # print(stack)
-
-    for _ in range(g):
-        # print(stack)
-        pivot_x, pivot_y=stack[-1][0], stack[-1][1]
-        # print(pivot_x, pivot_y)
-        graph[pivot_x][pivot_y]=1
-        
-        for i in range(num_point-1):
-            idx=num_point-2-i
-            pre_x, pre_y=stack[idx][0], stack[idx][1]
-            # graph[pre_x][pre_y]=1
-
-            new_x=pivot_x+(pre_y-pivot_y)
-            new_y=pivot_y-(pre_x-pivot_x)
-            # print((pre_x, pre_y), (new_x, new_y))
-            if 0<=new_x<=size-1 and 0<=new_y<=size-1:
-                graph[new_x][new_y]=1
-                stack.append((new_x, new_y))
-
-        num_point=num_point*2-1
-        
+GRID_SIZE = 101
+grid = [[0 for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
 
-r,c=0,0
-cnt=0
-for col in range(size-1):
-    for row in range(size-1):
-        sum_=graph[row][col]+graph[row+1][col]\
-            +graph[row][col+1]+graph[row+1][col+1]
-        if sum_==4:
-            cnt+=1
-print(cnt)
+def make_dragon_curve(data):
+    for y, x, d, g in data:
 
-        
-        
+        curve_points = []
+        curve_points.append((x, y))
+        curve_points.append((x + dx[d], y + dy[d]))
 
+        grid[x][y] = 1
+        grid[x + dx[d]][y + dy[d]] = 1
+
+        for _ in range(g):
+            pivot_x, pivot_y = curve_points[-1]
+
+            for i in range(len(curve_points) - 2, -1, -1):
+                pre_x, pre_y = curve_points[i]
+                new_x = pivot_x + (pre_y - pivot_y)
+                new_y = pivot_y - (pre_x - pivot_x)
+
+                if 0 <= new_x <= GRID_SIZE - 1 and 0 <= new_y <= GRID_SIZE - 1:
+                    grid[new_x][new_y] = 1
+                    curve_points.append((new_x, new_y))
+
+
+def count_squares():
+    cnt = 0
+    for col in range(GRID_SIZE - 1):
+        for row in range(GRID_SIZE - 1):
+            sum_ = (
+                grid[row][col]
+                + grid[row + 1][col]
+                + grid[row][col + 1]
+                + grid[row + 1][col + 1]
+            )
+            if sum_ == 4:
+                cnt += 1
+
+    return cnt
+
+
+make_dragon_curve(data)
+print(count_squares())
